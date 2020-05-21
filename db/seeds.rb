cfa58192855e020c94c91cfb67d57ff522c63c85
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-21 17:26:58 -0400

    seed file get not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "rest-client"
+require "byebug"
+require 'uri'
+
+
+key = ENV["API_KEY"]
+# res = RestClient.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key=#{key} HTTP/1.1`)
+uri = URI(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key=#{key} HTTP/1.1`)
+data = JSON.parse(uri)
+byebug
