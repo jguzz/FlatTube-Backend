ab0ed6706163a6cb4713539123f6ea5e9b60fda3
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-22 11:17:56 -0400

    index updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "rest-client"
+require "byebug"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,13 +7,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "rest-client"
-require "byebug"
-require 'uri'
+# require 'uri'
 
 
 key = ENV["API_KEY"]
-# res = RestClient.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key=#{key} HTTP/1.1`)
-uri = URI(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&key=#{key} HTTP/1.1`)
-data = JSON.parse(uri)
-byebug
+res = RestClient.get("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics%2Cplayer&chart=mostPopular&key=#{key}")
+data = JSON.parse(res)
+video = data[0]
+data.each do |obj| 
+	obj.each do |key, val|
+		if key === "snippet"
+			key.each do |snipKey, snipId|
+				if snipKey
+			end
+		end
+	end
+end
+# puts "#{data[0]["snippet"]}"
+puts data["items"]
