4c89750f6d9228d85977d9e72ca9a9ab19f477df
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-27 13:22:04 -0400

    like controler has index,show,create,update,delete

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,47 @@
 class LikesController < ApplicationController
+		before_action :find_like,only: [:show, :edit, :update]
+	
+		def index
+			@likes=Like.all
+			render json:@likes
+		end
+	
+		def show
+			render json:@like
+		end
+		
+
+	
+		def create
+			@like= Like.new(like_params)
+			@like.save
+	
+			render json:@like
+		end
+		 
+		def update
+			@like.update(params.require(:like).permit!)
+			if @like.valid?
+				render json:@like
+			else
+				flash[:errors]=@like.erors.full_messages
+			end
+		end
+	  
+		  def delete 
+				Like.find(params[:id]).delete
+			end
+	
+		private
+	
+		def like_params
+			params.require(:like).permit!
+		end
+	
+		def find_like
+			@like = Like.find(params[:id])
+		end
+	
+	
+
 end
